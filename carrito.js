ecf7300c988a0007a97553150801589c60b0c3fc
PleaseDo <PleaseDo01@hotmail.com>
2021-04-20 10:20:26 -0500

    Carrito modulo de control completo

--- carrito.js
+++ carrito.js
@@ -2,23 +2,28 @@
 
 //Seleccionamos todos los elementos con la clase 'addToCart'
 const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
-console.log('addToShoppingCartButtons', addToShoppingCartButtons);
+// console.log('addToShoppingCartButtons', addToShoppingCartButtons);
 
 //Añadimos un evento por cada botón del carrito
 addToShoppingCartButtons.forEach(addToCartButton => {
     addToCartButton.addEventListener('click', addToCartClicked);
 });
 
-const buyButton = document.querySelector('.comprarButton');
-buyButton.addEventListener('click', buyButtonClicked);
+const discountInputElement = document.querySelector('.discountTotal');
+discountInputElement.addEventListener('keyup', discountInputKeyup);
+
+
 
 const voidButton = document.querySelector('.vaciarButton');
 voidButton.addEventListener('click', voidButtonClicked);
 
-const discountInputElement = document.querySelector('.discountTotal');
-discountInputElement.addEventListener('keyup', discountInputKeyup);
+const buyButton = document.querySelector('.pagarButton');
+buyButton.addEventListener('click', pagarButtonClicked);
+
+
 
 const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
+
 
 
 //Recibimos el evento en la función
@@ -90,11 +95,22 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage){
 
 function updateShoppingCartTotal(discountInput){
     let total = 0;
-    //Muestra el valor en pantalla (Lo imprime donde toma la clase)
+    let total2 = 0;
+
+    //Selecciona el elemento por la clase
     const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+    const shoppingCartTotalModal = document.querySelector('.shoppingCartTotalModal');
+
+    //Muestra el valor en pantalla en el Modal
+    const shoppingCartQuantity = document.querySelector('.shoppingCartQuantity');
+
+    //Tomamos la clase de notificaciones del botón de arriba
+    const notifQuantity = document.querySelector('.notifQuantity');
+
     // Seleccionamos todos los elementos con la clase shoppingCartItem
     const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
 
+    let quantity = 0;
 
     shoppingCartItems.forEach((shoppingCartItem) => {
         // Tomamos el Precio por cada Item
@@ -106,29 +122,37 @@ function updateShoppingCartTotal(discountInput){
         //Tomamos el valor extraido de shoppingCartItemQuantityElement
         const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
         
+        //Tomamos la cantidad de productos para mostrarsela al usuario.
+        quantity = quantity + shoppingCartItemQuantity;
+
         //Operación de nuestro precio total
-        
-            
             total = total + shoppingCartItemPrice * shoppingCartItemQuantity;    
+            total2 = total;
             if(total < 0){
                 total = 0
             }
        
     });
-
         
 
         if(discountInput > 0){
-            total = total - discountInput;
-            if(total < 0)
+            total2 = total - discountInput;
+            if(total2 < 0)
             {
-                total = 0
+                total2 = 0
             }
         }
-        
-    
+      
     //Mostramos nuestro valor total de la operación
     shoppingCartTotal.innerHTML = `${total.toFixed(2)} $MXN`
+    shoppingCartTotalModal.value = `${total2.toFixed(2)} $MXN`
+
+
+    notifQuantity.innerHTML = `<span class="badge badge-light"> ${quantity}</span>`;
+    shoppingCartQuantity.value = ` ${quantity}`;
+   
+      
+        
 }
 
 function removeShoppingCartItem(event){
@@ -150,10 +174,7 @@ function quantityChanged(event){
     updateShoppingCartTotal();
 }
 
-function buyButtonClicked(){
-    shoppingCartItemsContainer.innerHTML = '';
-    updateShoppingCartTotal();
-}
+
 
 //Funcion para vaciar el carrito
 function voidButtonClicked(){
@@ -161,10 +182,102 @@ function voidButtonClicked(){
     updateShoppingCartTotal();  
 }
 
-//Funcion para vaciar el carrito
+//Funcion para vaciar el carrito pagando
+function pagarButtonClicked(){
+    shoppingCartItemsContainer.innerHTML = '';
+    updateShoppingCartTotal();  
+}
+
+function checkchange(){
+    var checkBox = document.getElementById("Checked");
+    const generate = document.querySelector('.generateTotal');
+    if (checkBox.checked == true){
+        generate.innerHTML = `Monedas de descuento (99 máximo):
+        <input class="form-control small shoppingCartTotalModal discountTotal" id='discount' type="text">`;
+
+      } else {
+        generate.innerHTML = ''
+      }
+}
+
+//Funcion para aplicar descuento
 function discountInputKeyup(){
     let discountInput = 0;
     discountInput = Number(discountInputElement.value)
+
+    var input=  document.getElementById('discount');
+    input.addEventListener('input',function(){
+    if (this.value.length > 2) 
+        this.value = this.value.slice(0,2); 
+    })
     // console.log(discountInput)
     updateShoppingCartTotal(discountInput);
 }
+
+//Función Mouseover
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+
+
+
+
+
+function cargarProvincias() {
+    var array = ["Edomex", "Jalisco", "Michoacán", "Chiapas", "Guerrero"];
+    array.sort();
+    addOptions("provincia", array);
+}
+
+
+//Función para agregar opciones a un <select>.
+function addOptions(domElement, array) {
+    var selector = document.getElementsByName(domElement)[0];
+    for (provincia in array) {
+        var opcion = document.createElement("option");
+        opcion.text = array[provincia];
+        // Añadimos un value a los option para hacer mas facil escoger los pueblos
+        opcion.value = array[provincia].toLowerCase()
+        selector.add(opcion);
+    }
+}
+
+
+
+function cargarPueblos() {
+    // Objeto de provincias con pueblos
+    var listaPueblos = {
+      edomex: ["Toluca", "Valle de Bravo", "San Juan", "Aculco", "Ecatepec"],
+      jalisco: ["Guadalajara", "Mazamitla", "Tapalpa", "Lagos de Moreno", "San Pedro"],
+      michoacan: ["Morelia", "Pátzcuaro", "Santa Clara", "Uruapan", "Zamora de Hidalgo"],
+      chiapas: ["Tuxtla ", "San Pedro", "Tapachula", "Palenque", "Chiapa de Corzo"],
+      guerrero: ["Acapulco", "Taxco", "Tlapa", "Teloloapan", "Pilcaya"]
+    }
+    
+    var provincias = document.getElementById('provincia')
+    var pueblos = document.getElementById('pueblo')
+    var provinciaSeleccionada = provincias.value
+    
+    // Se limpian los pueblos
+    pueblos.innerHTML = '<option value="">Seleccione un Pueblo...</option>'
+    
+    if(provinciaSeleccionada !== ''){
+      // Se seleccionan los pueblos y se ordenan
+      provinciaSeleccionada = listaPueblos[provinciaSeleccionada]
+      provinciaSeleccionada.sort()
+    
+      // Insertamos los pueblos
+      provinciaSeleccionada.forEach(function(pueblo){
+        let opcion = document.createElement('option')
+        opcion.value = pueblo
+        opcion.text = pueblo
+        pueblos.add(opcion)
+      });
+    }
+    
+  }
+  cargarProvincias();
